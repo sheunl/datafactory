f61a9c7e90640c00e803c7141bb99117791af5f3
Abdulrasaq Lawani <abdulrasaqolawani@gmail.com>
2024-09-12 06:05:46 -0400

    Added CLI library

--- src/core.go
+++ src/core.go
@@ -3,11 +3,44 @@ package main
 import (
 	"datafactory/generators"
 	"fmt"
+	"github.com/jroimartin/gocui"
+	"log"
 )
 
 func main() {
 
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+
 	ints := generators.GenerateIntegers(10, 9, 90, "")
 	fmt.Println(ints)
 
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
--- src/generators/numbers.go
+++ src/generators/numbers.go
@@ -1,7 +1,6 @@
 package generators
 
 import (
-	"fmt"
 	"math/rand"
 )
 
